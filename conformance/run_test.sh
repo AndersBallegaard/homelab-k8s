18c364e509f2efea9b45632f64280465260e8346
Anders Ballegaard <github@anderstb.dk>
2025-02-22 08:07:00 +0000

    switch to hydrophone

--- conformance/run_test.sh
+++ conformance/run_test.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 k8s_version=$(kubectl version -o json | jq .serverVersion.gitVersion)
 prod_name=srv6-homelab
+mkdir -p ./${k8s_version}/
 
-go install github.com/vmware-tanzu/sonobuoy@latest
+go install sigs.k8s.io/hydrophone@latest
 
-~/go/bin/sonobuoy run --mode=certified-conformance --wait
-outfile=$(~/go/bin/sonobuoy retrieve)
-mkdir ./results; tar xzf $outfile -C ./results
-rm -rf ${k8s_version}
-mkdir -p ./${k8s_version}/${prod_name}
-cp ./results/plugins/e2e/results/global/* ./${k8s_version}/${prod_name}/
+~/go/bin/hydrophone --conformance --output-dir ./${k8s_version}/
 
 echo "Remember to commit these results, please specify if it was done as part of an upgrade, cluster rebuild, or improvement"
