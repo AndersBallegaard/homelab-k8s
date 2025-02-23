0f3b7677123edcd840394b564fe913cdaaf86ece
Anders Ballegaard <github@anderstb.dk>
2025-02-23 08:13:04 +0000

    Delete Cinder from git assets

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+cd assets
+rm -rf cinder/
+wget https://github.com/chrissimpkins/cinder/archive/v1.2.0.zip
+unzip v1.2.0
+mv cinder-1.2.0/cinder/ .
+rm -rf v1.2.0.zip
+rm -rf cinder-1.2.0
+cd ..
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
